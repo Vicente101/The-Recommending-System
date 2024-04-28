8fa86b3a437688a07d0ff2978c6c37813d8e2131
Karter888 <122399969+Karter888@users.noreply.github.com>
2024-04-28 19:05:27 +0200

    SavePoint 10

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -20,3 +20,15 @@ $sunrise = $_POST['sunrise'];
 $sunset = $_POST['sunset'];
 $wind_speed = $_POST['wind_speed'];
 
+// Insert data into the database table
+$sql = "INSERT INTO weather_data(humidity, pressure, sunrise, sunset, wind_speed)
+VALUES ('$humidity', '$pressure', '$sunrise', '$sunset', '$wind_speed')";
+
+if ($conn->query($sql) === True){
+   echo " Weather data inserted successfully";
+}  else{ 
+   echo "Error:" . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
